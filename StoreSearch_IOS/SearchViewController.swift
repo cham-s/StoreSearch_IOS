23222ef28cd757f1ee39cc60991e55a92813c003
cham-s <cattouma@student.42.fr>
2016-06-05 22:29:37 +0200

    connect view with table and searchbar

---
+++ StoreSearch_IOS/SearchViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  StoreSearch_IOS
+//
+//  Created by cham-s on 05/06/16.
+//  Copyright © 2016 StellarTech Media. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableview: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableview.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    // MARK - Custom Methods
+    
+    
+    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        print("The search text is '\(searchBar.text)'")
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
