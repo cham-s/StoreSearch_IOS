e056fcdafce0fe2b8711d8a36ee606b7b38bbbee
cham-s <cattouma@student.42.fr>
2016-06-06 07:28:43 +0200

    added asset file, not found xib

---
+++ StoreSearch_IOS/SearchResultCell.swift
@@ -0,0 +1,28 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch_IOS
+//
+//  Created by cham-s on 06/06/16.
+//  Copyright © 2016 StellarTech Media. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artWork: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- StoreSearch_IOS/SearchViewController.swift
+++ StoreSearch_IOS/SearchViewController.swift
@@ -13,15 +13,14 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableview: UITableView!
     var hasSearch = false
-    var searchResults = [SearchResult]() {
-        didSet {
-            tableview.reloadData()
-        }
-    }
-    
-
+    var searchResults = [SearchResult]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        let cellNib = UINib(nibName: TabbleViewIdentifiers.searchResultCell, bundle: nil)
+        let cellNotNib = UINib(nibName: TabbleViewIdentifiers.nothingFoundCell, bundle: nil)
+        tableview.registerNib(cellNib, forCellReuseIdentifier: TabbleViewIdentifiers.searchResultCell)
+        tableview.registerNib(cellNotNib, forCellReuseIdentifier: TabbleViewIdentifiers.nothingFoundCell)
+        tableview.rowHeight = 80
         tableview.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
@@ -32,6 +31,12 @@ class SearchViewController: UIViewController {
 
 
     // MARK - Custom Methods
+    
+    // MARK - Custom Struct
+    struct TabbleViewIdentifiers {
+        static let searchResultCell = "SearchResultCell"
+        static let nothingFoundCell = "NothingFoundCell"
+    }
     
     
     
@@ -50,8 +55,10 @@ extension SearchViewController: UISearchBarDelegate {
                 searchResult.artistName = searchBar.text!
                 searchResults.append(searchResult)
             }
-            hasSearch = true
+            
         }
+        hasSearch = true
+        tableview.reloadData()
     }
     
     func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
@@ -78,20 +85,16 @@ extension SearchViewController: UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cellID = "SearchResultCell"
-        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellID)
-        if cell == nil {
-            cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellID)
-        }
+        
         if searchResults.count == 0 {
-            cell.textLabel!.text = "(Nothing Found)"
-            cell.detailTextLabel!.text = ""
+            return tableview.dequeueReusableCellWithIdentifier(TabbleViewIdentifiers.nothingFoundCell, forIndexPath: indexPath)
         } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier( TabbleViewIdentifiers.searchResultCell, forIndexPath: indexPath) as! SearchResultCell
             let searchResult = searchResults[indexPath.row]
-            cell.textLabel!.text = searchResult.name
-            cell.detailTextLabel!.text = searchResult.artistName
+            cell.nameLabel!.text = searchResult.name
+            cell.artistNameLabel!.text = searchResult.artistName
+            return cell
         }
-        return cell
     }
 }
 
