caf7047977ad43525ace0880b93e717730922d64
cham-s <cattouma@student.42.fr>
2016-06-11 09:17:42 +0200

    added new viewcontroller

---
+++ StoreSearch_IOS/LandscapeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LandscapeViewController.swift
+//  StoreSearch_IOS
+//
+//  Created by Chamsidine ATTOUMANI on 6/11/16.
+//  Copyright © 2016 StellarTech Media. All rights reserved.
+//
+
+import UIKit
+
+class LandscapeViewController: UIViewController {
+    
+}
